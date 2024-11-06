616d6c2dc13a0b7cd44c0111193f225f0cdbb5df
Hacanna42 <dtmdgus901@gmail.com>
2024-11-06 22:23:19 +0900

    feat(OutputView): 구매 요청 메시지 출력 기능 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,11 +1,15 @@
 package store.view;
 
 public class OutputView {
-    public void printGreetingMessage() {
+    protected void printGreetingMessage() {
         System.out.println("안녕하세요. W편의점입니다.");
     }
 
-    public void printStockStatus(/* 이곳에 재고 역할의 객체 추가 */) {
+    protected void printBuyRequestMessage() {
+        System.out.println("구매하실 상품명과 수량을 입력해 주세요.");
+    }
+
+    protected void printStockStatus(/* 이곳에 재고 역할의 객체 추가 */) {
         printStockNoticeMessage();
         // TODO: 재고 역할에서 재고 정보를 얻어오기
     }
