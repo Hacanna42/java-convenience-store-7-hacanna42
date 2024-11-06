caeb69b3d77fe9b67e75cbcf3ab4defbdf71aaf2
Hacanna42 <dtmdgus901@gmail.com>
2024-11-06 22:14:37 +0900

    feat(OutputView): 재고 정보 출력 기능 일부 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -4,4 +4,13 @@ public class OutputView {
     public void printGreetingMessage() {
         System.out.println("안녕하세요. W편의점입니다.");
     }
+
+    public void printStockStatus(/* 이곳에 재고 역할의 객체 추가 */) {
+        printStockNoticeMessage();
+        // TODO: 재고 역할에서 재고 정보를 얻어오기
+    }
+
+    private void printStockNoticeMessage() {
+        System.out.println("현재 보유하고 있는 상품입니다.");
+    }
 }
