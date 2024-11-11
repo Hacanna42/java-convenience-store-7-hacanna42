f80cefce809104ad53a18b0ce5400568f21756ef
Hacanna42 <dtmdgus901@gmail.com>
2024-11-11 18:00:45 +0900

    refactor(Promotions): 일급 컬렉션의 생성자에 대해 방어적 복사 적용

--- src/main/java/store/domain/promotion/Promotions.java
+++ src/main/java/store/domain/promotion/Promotions.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Promotions 는 Promotion 의 일급 컬렉션입니다.
+ * @see Promotion
+ */
 public class Promotions {
     private final List<Promotion> promotions;
 
     public Promotions(List<Promotion> promotions) {
-        this.promotions = promotions;
+        this.promotions = List.copyOf(promotions);
     }
 
     public Optional<Promotion> findPromotionByName(String promotionName) {
